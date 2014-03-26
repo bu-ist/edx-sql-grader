6ee0a0ba9da47a0a0a866e27b30957d452ed081c
Mike Burns <mgburns@bu.edu>
2014-03-26 15:03:56 -0400

    Switch to dictConfig for logging configuration.
    Adds console log handler for development.

--- run.py
+++ run.py
@@ -1,11 +1,45 @@
 #!/usr/bin/env python
-import logging
+import logging.config
 
 import settings
 from graders import GraderDaemon
 
+LOG_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'default': {
+            'format': settings.LOG_FORMAT,
+            },
+        },
+
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'default'
+            },
+        'file': {
+            'level': settings.LOG_LEVEL,
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'default',
+            'filename': settings.DAEMON_LOG,
+            'maxBytes': 20480,
+            'backupCount': 3,
+            },
+        },
+
+    'loggers': {
+        '': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+            'propagate': False
+            }
+    }
+}
+
+
 if __name__ == "__main__":
-    logging.basicConfig(level=settings.LOG_LEVEL,
-                        filename=settings.DAEMON_LOG,
-                        format=settings.LOG_FORMAT)
+    logging.config.dictConfig(LOG_CONFIG)
     grader = GraderDaemon().start()
